496d6abc0d1f2a187c6761c2ca887eec1daa790c
DevMasteryLabs <develop.devmastery@gmail.com>
2022-03-28 23:11:37 +0100

    Refactor some multer code

--- middlewares/multer-file-upload/cloudinary.js
+++ middlewares/multer-file-upload/cloudinary.js
@@ -1,18 +1,9 @@
 const multer = require('multer');
-
-const MIME_TYPE_MAP = {
-  'image/png': 'png',
-  'image/jpeg': 'jpeg',
-  'image/jpg': 'jpg'
-};
+const {fileFilter} = require('../../utils/multer');
 
 const fileUpload = multer({
   storage: multer.diskStorage({}),
-  fileFilter: (req, file, cb) => {
-    const isValid = !!MIME_TYPE_MAP[file.mimetype];
-    let error = isValid ? null : new Error('Invalid file type!');
-    cb(error, isValid);
-  }
+  fileFilter: fileFilter
 });
 
 const upload = fileUpload.single('image'); // because in form data we named the file field 'image'
@@ -20,12 +11,14 @@ const upload = fileUpload.single('image'); // because in form data we named the 
 module.exports = (req, res, next) => {
     upload(req, res, (err) => {
         if (err instanceof multer.MulterError || err) {
+            // A Multer error occurred when uploading || An unknown error occurred when uploading
             return res.status(400).json({
                 type: 'error',
                 message: err.message,
                 details: err
             })
-        } 
+        }
+        // Everything went fine
         next();
     })
 }
--- middlewares/multer-file-upload/local.js
+++ middlewares/multer-file-upload/local.js
@@ -1,28 +1,19 @@
 const multer = require('multer');
 const path = require('path');
 const { v4: uuidv4 } = require('uuid');
+const {fileFilter} = require('../../utils/multer');
 
-const MIME_TYPE_MAP = {
-  'image/png': 'png',
-  'image/jpeg': 'jpeg',
-  'image/jpg': 'jpg'
-};
+const storage = multer.diskStorage({
+  destination: `${__dirname}/../../public/uploads`,
+  filename: (req, file, cb) => {
+    // cb() can be named done()
+    cb(null, uuidv4() + path.extname(file.originalname))
+  }
+});
 
 const fileUpload = multer({
-  storage: multer.diskStorage({
-    destination: (req, file, cb) => {
-        // cb() can be named done()
-        cb(null, `${__dirname}/../../public/uploads`);
-    },
-    filename: (req, file, cb) => {
-        cb(null, uuidv4() + path.extname(file.originalname))
-    }
-  }),
-  fileFilter: (req, file, cb) => {
-    const isValid = !!MIME_TYPE_MAP[file.mimetype];
-    let error = isValid ? null : new Error('Invalid file type!');
-    cb(error, isValid);
-  }
+  storage: storage,
+  fileFilter: fileFilter
 });
 
 const upload = fileUpload.single('image'); // because in form data we named the file field 'image' 
@@ -30,12 +21,14 @@ const upload = fileUpload.single('image'); // because in form data we named the 
 module.exports = (req, res, next) => {
     upload(req, res, (err) => {
         if (err instanceof multer.MulterError || err) {
+            // A Multer error occurred when uploading || An unknown error occurred when uploading
             return res.status(400).json({
                 type: 'error',
                 message: err.message,
                 details: err
             })
-        } 
+        }
+        // Everything went fine
         next();
     })
 }
